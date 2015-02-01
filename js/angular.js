7d0c27c6131552b79f6ac91e842ddcaa79e447df
Geoff <geoffrey.brown@mac.com>
2015-02-01 23:44:59 +0000

    More Changes

--- js/angular.js
+++ js/angular.js
@@ -8,21 +8,59 @@
 
 	});
 
-	app.controller('TabController', function () {
-		this.tab = 1;
-		this.setTab = function (newvalue) {
-			this.tab = newvalue;
-		};
-		this.isSet = function (valuetocheck) {
-			return (valuetocheck === this.tab);
-		};
-	});
-
 	app.controller('GalleryController', function () {
 		this.current = 0;
 
 		this.setCurrent = function (newval) {
 			this.current = (newval === null) ? 0 : newval;
+		};
+	});
+
+	app.controller('ReviewController', function () {
+		this.review = {};
+
+		this.addReview = function (product) {
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
+	app.directive("productTabs", function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-tabs.html',
+			controller: function () {
+				this.tab = 1;
+				this.isSet = function (checkTab) {
+					return this.tab === checkTab;
+				};
+				this.setTab = function (setTab) {
+					this.tab = setTab;
+				};
+			},
+			controllerAs: 'tabCtrl'
+		};
+	});
+
+	app.directive("productDescription", function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-description.html'
+		};
+	});
+
+	app.directive("productSpecification", function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-specification.html'
+		};
+	});
+
+	app.directive("productReviews", function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-reviews.html'
 		};
 	});
 
